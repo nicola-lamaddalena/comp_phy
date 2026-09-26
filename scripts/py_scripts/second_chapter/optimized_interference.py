0614591150b5983303bca6d9b3f98cd749f1d3a2
nico <niks9498@gmail.com>
2026-09-26 12:34:47 +0200

    minor commit

--- scripts/py_scripts/second_chapter/optimized_interference.py
+++ scripts/py_scripts/second_chapter/optimized_interference.py
@@ -1,14 +1,14 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-ROW_LEN = 1024
-LAM = 15
+ROW_LEN = 2048
+LAM = 50
 K = 2 * np.pi / LAM
-AMPLITUDE = 10
+AMPLITUDE = 1
 
 sources = np.array([
-    [512, 412],
-    [512, 612],
+    [ROW_LEN/2, ROW_LEN/2 - ROW_LEN/10],
+    [ROW_LEN/2, ROW_LEN/2 + ROW_LEN/10],
 ])
 
 # Separazione automatica di coordinate X e Y per tutte le sorgenti
@@ -20,12 +20,13 @@ y = np.arange(0, ROW_LEN)
 X, Y = np.meshgrid(x, y, indexing='ij')
 
 # Calcolo adattivo indipendente dal numero di sorgenti
-dx = X[:, :, np.newaxis] - sources_x  # Forma: (256, 256, N_sources)
-dy = Y[:, :, np.newaxis] - sources_y  # Forma: (256, 256, N_sources)
+dx = X[:, :, np.newaxis] - sources_x  # Forma: (ROW_LEN, ROW_LEN, N_sources)
+dy = Y[:, :, np.newaxis] - sources_y  # Forma: (ROW_LEN, ROW_LEN, N_sources)
 R = np.sqrt(dx**2 + dy**2)
 
 # La somma lungo axis=2 somma automaticamente tutte le N sorgenti presenti
 LATTICE = np.sum(AMPLITUDE * np.sin(K * R), axis=2)
 
 plt.imshow(LATTICE, origin="lower")
+plt.inferno()
 plt.show()
